c0f380c23b50a91cb0b8797bab0994d0289292bb
Raman Agrawal <iramanagrawal@gmail.com>
2024-05-05 22:44:32 +0530

    we do

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar/Navbar";
+import Whatwedo from "@/components/wedo/Whatwedo";
 import Image from "next/image";
 
 export default function Home() {
   return (
   <>
   <Navbar/>
+  <Whatwedo/>
   </>
   );
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,10 +1,49 @@
+import Image from "next/image";
 import React from "react";
+import Button from "../Button";
+import Link from "next/link";
 
 const Navbar = () => {
+  const demos = [
+    {
+      imgSrc: "/demos/1.jpg",
+      imgSrcset: "/demos/1.jpg 1x",
+    },
+  ];
   return (
-<nav>
-  
-</nav>
+    <nav className="h-24 fixed w-full flexCenter  bg-[#372b70]">
+      <div className="container flexBetween">
+        <div className="relative h-10 w-40">
+          <Image src="/logo-light@2x.png" alt="logo" fill />
+        </div>
+        <div className="flexBetween gap-10">
+          <ul className="flexBetween gap-10 font-bold text-white">
+            <li className="relative group">
+              <Link href="#">Demos</Link>
+
+              <div className="absolute invisible translate-y-96 bg-black group-hover:-translate-y-80  group-hover:visible h-lvh -right-[43vw] w-[90vw] container border"></div>
+            </li>
+            <li className="relative">
+              <Link href="#">Pages</Link>
+            </li>
+            <li className="relative">
+              <Link href="#">Projects</Link>
+            </li>
+            <li className="relative">
+              <Link href="#">Blogs</Link>
+            </li>
+            <li className="relative">
+              <Link href="#">Blocks</Link>
+            </li>
+            <li className="relative">
+              <Link href="#">Documentation</Link>
+            </li>
+          </ul>
+          <Button title="Free Trial" />
+        </div>{" "}
+      </div>
+    </nav>
+
 
   );
 };
