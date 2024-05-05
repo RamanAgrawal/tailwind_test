dec267083207d825ad13ba2f547b654908219d7d
Raman Agrawal <iramanagrawal@gmail.com>
2024-05-05 22:48:51 +0530

    case

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import Case from "@/components/casestudies/Case";
 import Navbar from "@/components/navbar/Navbar";
 import Whatwedo from "@/components/wedo/Whatwedo";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <>
-  <Navbar/>
-  <Whatwedo/>
-  </>
+    <>
+      <Navbar />
+      <Whatwedo />
+      <Case />
+    </>
   );
 }
